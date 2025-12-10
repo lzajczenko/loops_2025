e7e6679c0a8e550153a526b4922897a7933efcfb
lzajczenko <lzajczenko@cps.edu>
2025-12-10 18:28:16 +0000

    list_in_range

--- 1_loops.py
+++ 1_loops.py
@@ -2,8 +2,9 @@
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
 
-# Challenge:
-# Use a for loop to print each fruit on a new line.
+print(len(fruits))
+
+# Challenge: Use a for loop to print each fruit on a new line.
 print(fruits[0])
 print(fruits[1])
 print(fruits[2])
@@ -11,12 +12,45 @@ for fruit in fruits:
     print(fruit)
 # i just worked with loops
 
+
+
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
-# Challenge:
-# Use a for loop and range to print each subject along with its index:
+
+#Print out each subject but stop when you reach "History"
+for subject in subjects:
+    if subject == "History":
+        break
+    print(subject)
+
+#Skip over "Science" and print the rest
+for subject in subjects:
+    if subject == "Science":
+        continue # skips over science but continues with rest of list
+    print(subject)
+
+
+
+
+#Stop at 600
+list1000 = list(range(1, 1001))
+for num in list1000:
+    if num > 599:
+        break
+    print(num)
+
+#Skip 300 to 500
+for num in list1000:
+    if (num <= 500) and (num >= 300):
+        continue
+    print(num)
+
+
+
+
+# Challenge: Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
 
 
